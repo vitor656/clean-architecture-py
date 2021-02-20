7d395e3c7ad131cfb782b476a119a474f876feb9
Vitor <vitor.rabelo.656@gmail.com>
2021-02-20 17:38:29 -0300

    Implementing select_pet in PetsRepository

--- src/infra/repo/pets_repository_test.py
+++ src/infra/repo/pets_repository_test.py
@@ -1,5 +1,7 @@
 from faker import Faker
 from src.infra.config import DBConnectionHandler
+from src.infra.entities.pets import AnimalTypes
+from src.infra.entities import Pets as PetsModel
 from .pets_repository import PetsRepository
 
 faker = Faker()
@@ -31,3 +33,37 @@ def test_insert_pet():
     assert new_pet.user_id == query_pet.user_id
 
     engine.execute("DELETE FROM pets WHERE id='{}'".format(new_pet.id))
+
+
+def test_select_pet():
+    """ Should select a pet in PetsRepository and compare it """
+
+    pet_id = faker.random_number(digits=4)
+    name = faker.name()
+    specie = "fish"
+    age = faker.random_number(digits=1)
+    user_id = faker.random_number()
+
+    specie_mock = AnimalTypes("fish")
+    data = PetsModel(id=pet_id, name=name, specie=specie_mock, age=age, user_id=user_id)
+
+    # SQL Commands
+
+    engine = db_connection_handler.get_engine()
+    engine.execute(
+        "INSERT INTO pets (id, name, specie, age, user_id) VALUES ('{}', '{}', '{}', '{}' ,'{}');".format(
+            pet_id, name, specie, age, user_id
+        )
+    )
+
+    query_pets1 = pets_repository.select_pet(pet_id=pet_id)
+    query_pets2 = pets_repository.select_pet(user_id=user_id)
+    query_pets3 = pets_repository.select_pet(pet_id=pet_id, user_id=user_id)
+
+    print(query_pets1)
+
+    assert data in query_pets1
+    assert data in query_pets2
+    assert data in query_pets3
+
+    engine.execute("DELETE FROM pets WHERE id='{}';".format(pet_id))
