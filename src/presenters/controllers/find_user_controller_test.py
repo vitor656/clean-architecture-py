6fb7b197a38eb16ad01edbdd4c199d87ab06a3da
Vitor <vitor.rabelo.656@gmail.com>
2021-02-24 21:46:46 -0300

    feat: Implementing first controller

---
+++ src/presenters/controllers/find_user_controller_test.py
@@ -0,0 +1,41 @@
+from faker import Faker
+from .find_user_controller import FindUserController
+from src.data.test import FindUserSpy
+from src.infra.test import UserRepositorySpy
+from src.presenters.helpers import HttpRequest
+
+faker = Faker()
+
+
+def test_handle():
+    """ Testing handle method """
+
+    find_user_use_case = FindUserSpy(UserRepositorySpy())
+    find_user_controller = FindUserController(find_user_use_case)
+    http_request = HttpRequest(query={"user_id": faker.random_number()})
+
+    response = find_user_controller.handle(http_request)
+
+    # Testing Inputs
+    assert find_user_use_case.by_id_param["user_id"] == http_request.query["user_id"]
+
+    # Testing Output
+    assert response.status_code == 200
+    assert response.body
+
+
+def test_handle_no_query_param():
+    """ Testing handle method """
+
+    find_user_use_case = FindUserSpy(UserRepositorySpy())
+    find_user_controller = FindUserController(find_user_use_case)
+    http_request = HttpRequest()
+
+    response = find_user_controller.handle(http_request)
+
+    # Testing Inputs
+    assert find_user_use_case.by_id_param == {}
+
+    # Testing Output
+    assert response.status_code == 400
+    assert "error" in response.body
---
+++ src/presenters/errors/http_errors.py
@@ -0,0 +1,13 @@
+class HttpErrors:
+    """ Class to define error in http calls """
+
+    @staticmethod
+    def error_422():
+        """ HTTP 422 """
+
+        return {"status_code": 422, "body": {"error": "Unprossessable Entity"}}
+
+    @staticmethod
+    def error_400():
+        """ HTTP 400 """
+        return {"status_code": 400, "body": {"error": "Bad Request"}}
