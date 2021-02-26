aac07178d496108ea548286447035c3a5429006a
Vitor <vitor.rabelo.656@gmail.com>
2021-02-26 18:07:21 -0300

    feat: Register user controller and flask adapter

---
+++ src/main/adapter/api_adapter.py
@@ -0,0 +1,23 @@
+from typing import Type
+from sqlalchemy.exc import IntegrityError
+from src.main.interface import RouteInterface as Route
+from src.presenters.helpers import HttpRequest, HttpResponse
+from src.presenters.errors import HttpErrors
+
+
+def flask_adapter(request: any, api_route: Type[Route]) -> any:
+    """Adapter pattern to Flask
+    :param - Flask request
+    :api_route - COmposite routes
+    """
+
+    http_request = HttpRequest(body=request.json)
+    try:
+        response = api_route.route(http_request)
+    except IntegrityError:
+        http_error = HttpErrors.error_409()
+        return HttpResponse(
+            status_code=http_error["status_code"], body=http_error["body"]
+        )
+
+    return response
--- src/main/routes/api_route.py
+++ src/main/routes/api_route.py
@@ -1,11 +1,29 @@
-from flask import Blueprint, jsonify
-
-# from src.main.composer import register_user_composer
+from flask import Blueprint, jsonify, request
+from src.main.composer import register_user_composer
+from src.main.adapter import flask_adapter
 
 api_routes_bp = Blueprint("api_routes", __name__)
 
 
-@api_routes_bp.route("/api", methods=["GET"])
-def something():
-    """ Teste """
-    return jsonify({"Hello": "World"})
+@api_routes_bp.route("/api/users", methods=["POST"])
+def register_user():
+    """ Register User route """
+    message = {}
+    response = flask_adapter(request=request, api_route=register_user_composer())
+
+    if response.status_code < 300:
+        message = {
+            "Type": "users",
+            "id": response.body.id,
+            "attributes": {"name": response.body.name},
+        }
+
+        return jsonify({"data": message}), response.status_code
+
+    # Handling Erros
+    return (
+        jsonify(
+            {"error": {"status": response.status_code, "title": response.body["error"]}}
+        ),
+        response.status_code,
+    )
